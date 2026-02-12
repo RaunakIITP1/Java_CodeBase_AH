74ba9e655a1d78c05f40c0f34a7b49456e9dd9ac
Raunak Kumar <raunak.iitp@zohomail.in>
2026-02-12 15:16:57 +0530

    Insertion Sort

---
+++ src/SortingAlgo/InsertionSort.java
@@ -0,0 +1,25 @@
+package SortingAlgo;
+
+public class InsertionSort {
+    static void main(String[] args) {
+        int [] arr= {25, 19, 27, 98, 112, 20};
+        int n = arr.length;
+
+
+        for (int i=1; i<n; i++){
+            int key = arr[i];
+            int j = i-1;
+
+            while(j>=0 && arr[j]> key)
+            {
+                arr[j+1]= arr[j];
+                j--;
+            }
+            arr[j+1]= key;
+        }
+
+        for(int x : arr){
+            System.out.print(x + " ");
+        }
+    }
+}
